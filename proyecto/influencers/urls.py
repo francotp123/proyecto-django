ad9b9e0da88246c1097d49b168521f8f33150e96
francotp123 <166016331+francotp123@users.noreply.github.com>
2024-05-31 13:41:49 -0300

    news views and their urls

--- proyecto/influencers/urls.py
+++ proyecto/influencers/urls.py
@@ -5,7 +5,9 @@ from influencers import views
 
 urlpatterns = [
     path('', views.mainpage, name='mainpage'),
-    path('upload/', views.upload, name='upload')
+    path('upload/', views.upload, name='upload'),
+    path('influencers/', views.influencers_main, name='Influencers_main'),
+    path('<int:influencer_id>/', views.influencer, name='influencer'),
 
 
 
