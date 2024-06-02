c79903378eb93064dfac76190531a873dd18db9f
francotp123 <166016331+francotp123@users.noreply.github.com>
2024-06-01 21:03:00 -0300

    correccion url y view

--- proyecto/influencers/urls.py
+++ proyecto/influencers/urls.py
@@ -6,6 +6,6 @@ from influencers import views
 urlpatterns = [
     path('', views.mainpage, name='mainpage'),
     path('upload/', views.upload, name='upload'),
-    path('top5', views.influencers_top5, name='Influencers_top5'),
+    path('top5/', views.influencers_top5, name='Influencers_top5'),
     path('<int:influencer_id>/', views.influencer, name='influencer'),
 ]
