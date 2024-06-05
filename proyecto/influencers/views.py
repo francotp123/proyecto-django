0ff76d25067be709f3641763a173350daeb1d5f2
francotp123 <166016331+francotp123@users.noreply.github.com>
2024-06-05 11:30:18 -0300

    view, html y url de tabla

--- proyecto/influencers/views.py
+++ proyecto/influencers/views.py
@@ -64,8 +64,12 @@ def influencers_top5(request):
 
 def influencer(request, influencer_id):
     return HttpResponse(f'Este es el influencer N° {influencer_id}')
-#def tabla(request):
-    
+def tabla(request):
+    influencers = Influencers.objects.all()
+    context = {
+        "influencers":influencers
+    }
+    return render(request, 'tabla2.html', context)
 def influencers_top10_avg(request):
     datos = Influencers.objects.order_by('-avg_eng')[:10]
 
