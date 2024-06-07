2d82acfe73998b2920101452f219aa37281deeca
francotp123 <166016331+francotp123@users.noreply.github.com>
2024-06-07 13:47:57 -0300

    view unificar categorias y y su url

--- proyecto/influencers/urls.py
+++ proyecto/influencers/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('Influencers_top5/', views.influencers_top5, name='Influencers_top5'),
     path('<int:influencer_id>/', views.influencer, name='influencer'),
     path('top10_avg/', views.influencers_top10_avg, name='top10_avg'),
-    path('tabla/', views.tabla, name='tabla')
+    path('tabla/', views.tabla, name='tabla'),
+    path('unificar_cat/', views.unificar_categorias_similares, name='unificar_cat')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
